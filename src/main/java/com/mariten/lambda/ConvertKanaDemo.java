103967b611031a7e44fe6d8abe022f3b337be9a0
Jeff Case <jeffrey.p.case@gmail.com>
2016-12-10 15:54:33 -0800

    Add handling of input JSON object to Lambda demo

--- src/main/java/com/mariten/lambda/ConvertKanaDemo.java
+++ src/main/java/com/mariten/lambda/ConvertKanaDemo.java
@@ -1,5 +1,7 @@
 package com.mariten.lambda;
 
+import java.util.HashMap;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
@@ -9,6 +11,14 @@ public class ConvertKanaDemo
     {
         LambdaLogger logger = lambda_context.getLogger();
         logger.log("Starting ConvertKanaDemo\n");
-        return "Blargh";
+
+        HashMap<String, String> input_params = (HashMap<String, String>) input;
+        if (input_params.containsKey("input_str")) {
+            String input_str = input_params.get("input_str");
+            logger.log("input_str: '" + input_str + "'\n");
+            return "Success";
+        } else {
+            return "Failure";
+        }
     }
 }
